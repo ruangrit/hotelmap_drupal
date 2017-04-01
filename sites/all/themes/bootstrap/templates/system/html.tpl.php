ba5a48fff54dc20dfa99c73ab3ff310226da13de
Rito <ruangrit@opendream.co.th>
2017-04-01 15:18:50 +0700

    compatible

--- sites/all/themes/bootstrap/templates/system/html.tpl.php
+++ sites/all/themes/bootstrap/templates/system/html.tpl.php
@@ -56,6 +56,7 @@
 ?><!DOCTYPE html>
 <html<?php print $html_attributes;?><?php print $rdf_namespaces;?>>
 <head>
+  <meta http-equiv="X-UA-Compatible" content="IE10">
   <link rel="profile" href="<?php print $grddl_profile; ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
